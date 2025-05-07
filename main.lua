221de14dbc09bdf7dde0380bf57217dda26549df
Spacyxxuu <leroyevan01@gmail.com>
2025-05-07 19:26:20 +0200

    main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+-- Chargement du GUI ESP
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Spacyxxuu/SpacyxxAimbot/main/esp_gui.lua"))()
+
+-- Chargement de l'aimbot
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Spacyxxuu/SpacyxxAimbot/main/aimbot.lua"))()
